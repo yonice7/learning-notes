b1dbf13c1101c9111da3dd949963765993d332bb
yonice7 <60485595+yonice7@users.noreply.github.com>
2023-06-12 10:43:43 -0500

    Add Code Challenge 2

--- codChallenges.js
+++ codChallenges.js
@@ -1,4 +1,4 @@
-/* Fist challenge
+// Fist challenge
 const game = {
   team1: {
     name: "Bayern Munich",
@@ -41,18 +41,17 @@ const game = {
     team2: 6.5,
   },
 };
+
 // 1
 const [players1, players2] = game.players;
 // 2
 [game.team1.gk, ...game.team1.fieldPlayers] = players1;
 [game.team2.gk, ...game.team2.fieldPlayers] = players2;
 
-
 console.log(game.team1.gk);
 console.log(game.team2.gk);
 console.log(game.team1.fieldPlayers);
 console.log(game.team2.fieldPlayers);
-
 
 // 3
 const allPlayers = players1.concat(players2);
@@ -61,10 +60,9 @@ const allPlayers = players1.concat(players2);
 // 4
 
 //First approach, not good
-const players1Final = players1.push("Thiago", "Coutinho", "Perisic");
-console.log(players1Final);
-console.log(players1);
-
+// const players1Final = players1.push("Thiago", "Coutinho", "Perisic");
+// console.log(players1Final);
+// console.log(players1);
 
 const players1Final = players1.concat("Thiago", "Coutinho", "Perisic");
 //console.log(players1Final);
@@ -105,7 +103,23 @@ printGoals2(...game.scored);
 // 7
 game.odds.team1 < game.odds.team2 &&
   console.log("Team 1 is more likely to win");
-*/
 
 // Second challenge
 // 1
+let theResult = `the goals were: `;
+for (const [i, player] of game.scored.entries()) {
+  theResult += `Goal ${i}: ${player}`;
+}
+
+// 2
+let oddAverage = 0;
+for (const odd of Object.values(game.odds)) {
+  oddAverage += odd;
+}
+console.log(oddAverage / oddAverage.lenght);
+
+// 3
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
