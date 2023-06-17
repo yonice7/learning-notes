6d9abdb3d8f1dbc7c8e784c75292dd12d8b8d2a4
yonice7 <60485595+yonice7@users.noreply.github.com>
2023-06-17 12:50:09 -0500

    Data structures overview

--- jsReview.js
+++ jsReview.js
@@ -485,6 +485,37 @@ console.log([...question.values()]); // w/out the dots it returns a MapIterator
   - Array or Sets (simple list?)
   - Object or Maps (key/value pairs?) --> keys allow us to describe values
 
+Other data structures in javascript are:
+- Built-In
+  - WeakMap
+  - WeakSet
+- Non-built in
+  - Stacks
+  - Queues
+  - Linked lists
+  - Trees
+  - Hash tables
+  
+Arrays vs Sets
+- Arrays
+  - Use when you need ordered list of values (might contain duplicates)
+  - Use when you need to manipulate data
+- Sets
+  - Use when you need to work with unique values
+  - Use when high-performance is really important
+  - Use to remove duplicates from arrays
 
-                                      
+Objects vs Maps
+- Objects
+  - More "traditional" key/value store ("abused" objects)
+  - Easier to write and access with . and []
+  - Use when you need to include functions (methods)
+  - Use when working with JSON (can convert to map)
+- Maps
+  - Better performance
+  - Keys can have any data type
+  - Easy to iterate
+  - Easy to compute
+  - Use when you simply need to map key to values
+  - Use when you need keys that are not strings
 */
