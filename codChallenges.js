f43636fafb3c7801222a02567e6cb207833779ad
yonice7 <60485595+yonice7@users.noreply.github.com>
2023-06-19 16:20:59 -0500

    Solving challenge #3

--- codChallenges.js
+++ codChallenges.js
@@ -123,3 +123,45 @@ for (const [team, odd] of Object.entries(game.odds)) {
   const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
   console.log(`Odd of ${teamStr} ${odd}`);
 }
+
+// Coding Challenge #3
+const gameEvents = new Map([
+  [17, "GOAL"],
+  [36, "Substitution"],
+  [47, "GOAL"],
+  [61, "Substitution"],
+  [64, "Yellow card"],
+  [69, "Red card"],
+  [70, "Substitution"],
+  [72, "Substitution"],
+  [76, "GOAL"],
+  [80, "GOAL"],
+  [92, "Yellow card"],
+]);
+
+// 1
+const events = [...new Set([gameEvents.values()])];
+console.log(events);
+
+// 2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4
+for (const [minute, event] of gameEvents.entries()) {
+  if (minute <= 45) {
+    console.log(`[FIRST HALF] ${minute}: ${event}`);
+  } else {
+    console.log(`[SECOND HALF] ${minute}: ${event}`);
+  }
+}
+// best way
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
