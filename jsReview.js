f1701157545375176610529602fdc65441b85f3c
Johnny Vergara <johnny@Johnnys-MacBook-Pro.local>
2023-12-16 16:39:45 -0300

    String methods practice, I took my time 🥵

--- jsReview.js
+++ jsReview.js
@@ -641,3 +641,19 @@ const planesInLine = function (n) {
 
 planesInLine(5);
 planesInLine(6);
+
+// String methods practice
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+for (const flight of flights.split("+")) {
+  const [type, from, to, time] = flight.split(";");
+  console.log(type);
+  const output = `${type.startsWith("_Delayed") ? "🔴" : ""}${type.replaceAll(
+    "_",
+    " "
+  )} from ${from.slice(0, 3).toUpperCase()} to ${to
+    .slice(0, 3)
+    .toUpperCase()} (${time.replace(":", "h")})`.padStart(43);
+  console.log(output);
+}
