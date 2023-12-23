93ea0157d77444c8611eed81ede96e02f2141ceb
yonice7 <60485595+yonice7@users.noreply.github.com>
2023-12-23 09:12:42 -0300

    more closure examples

--- jsReview.js
+++ jsReview.js
@@ -925,3 +925,15 @@ f(); // returns 1554
 console.dir(f);
 
 // Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
