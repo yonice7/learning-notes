d38f1ee56a1d70fe8ff6ba79571a1711f7e1a393
yonice7 <jvergara041@gmail.com>
2024-01-24 19:06:41 -0300

    operations with dates

--- jsReview.js
+++ jsReview.js
@@ -1316,3 +1316,12 @@ console.log(future.getMinutes());
 console.log(future.getSeconds());
 console.log(future.toISOString()); // international standard
 console.log(future.getTime()); // passed since the beginning of the Unix time
+// operations with dates
+const dateExample = new Date(2037, 10, 19, 15, 23, 5);
+// console.log(Number(dateExample));
+console.log(+dateExample); // convert to number and then convert back to date
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+console.log(days1);
